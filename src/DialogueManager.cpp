add2ffb9bd201a6c2f9218ddbff68250b8c9820e
ErikArgemi <erik.argemi@estudiantat.upc.edu>
2026-03-16 11:37:01 +0100

    Choices with text

--- src/DialogueManager.cpp
+++ src/DialogueManager.cpp
@@ -49,7 +49,7 @@ bool DialogueManager::LoadDialogs(std::string path, std::string fileName)
 }
 
 bool DialogueManager::ShowOptions(int node_value) {
-	if (node_value == 0) return 0;
+	/*if (node_value == 0) return 0;*/
 
 	SDL_Rect bt1Pos = { 520, 350, 120,20 };
 	std::dynamic_pointer_cast<UIButton>(Engine::GetInstance().uiManager->CreateUIElement(UIElementType::BUTTON, 1, GetTextFromNode(node_value), bt1Pos, this));
@@ -62,16 +62,18 @@ bool DialogueManager::ShowOptions(int node_value) {
 
 	SDL_Rect bt4Pos = { 720, 400, 120,20 };
 	std::dynamic_pointer_cast<UIButton>(Engine::GetInstance().uiManager->CreateUIElement(UIElementType::BUTTON, 4, GetTextFromNode(node_value+3), bt4Pos, this));
+
+	return true;
 }
 
 const char* DialogueManager::GetTextFromNode(int node_value) {
 	const char* ret = "Couldn't find the text.";
 	for (pugi::xml_node node = dialogsFileXML.child("dialogs").child("text"); node != NULL; node = node.next_sibling("text"))
 	{
-		LOG("Node id value: %i", node.attribute("id").as_int());
-		if (node.attribute("id").as_int() == node_value)
+		LOG("Node id value: %i", node.child("id").attribute("value").as_int());
+		if (node.child("id").attribute("value").as_int() == node_value)
 		{
-			ret = (const char*)node.attribute("content").as_string();
+			ret = (const char*)node.child("content").attribute("value").as_string();
 			LOG("Text from node assigned.");
 		}
 	}
