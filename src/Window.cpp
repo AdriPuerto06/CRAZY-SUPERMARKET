81b96f8fb8c012982eae35290ab938a49f0717d5
AdriaPuerto04 <adriapuerto06@gmail.com>
2026-03-28 13:04:54 +0100

    Game Resolution 2.0 (read desc if there are problems)
    If you have one problem when rendering something it may be because the function may be using the Window resolution instead of the Base resolution. something like this:
    WindowSize = Engine::GetInstance().window->GetWindowSize();
    int scale = Engine::GetInstance().window->GetScale();
     try changing the resolution to int baseW = Engine::GetInstance().window->GetBaseWidth();
    			int baseH = Engine::GetInstance().window->GetBaseHeight();
    
    also try to change the scale: float scaleX = WindowW / Engine::GetInstance().window->GetBaseWidth();
    	float scaleY = WindowY / Engine::GetInstance().window->GetBaseHeight();

--- src/Window.cpp
+++ src/Window.cpp
@@ -39,6 +39,9 @@ bool Window::Awake()
 		height = configParameters.child("resolution").attribute("height").as_int();
 		scale = configParameters.child("resolution").attribute("scale").as_int();
 
+		baseWidth = width;
+		baseHeight = height;
+
 		if (fullscreen == true)        flags |= SDL_WINDOW_FULLSCREEN;
 		if (borderless == true)        flags |= SDL_WINDOW_BORDERLESS;
 		if (resizable == true)         flags |= SDL_WINDOW_RESIZABLE;
@@ -128,13 +131,17 @@ bool Window::SetFullSize() {
 	return true;
 }
 
-bool Window::SetWindowed() {
+bool Window::SetWindowed(int scaleFactor) {
 
 	SDL_SetWindowFullscreen(window, false);
-	SDL_SetWindowSize(window, 640, 360);
-	width = 640;
-	height = 360;
-	SDL_SyncWindow(window);
+
+	int newWidth = baseWidth / scaleFactor;
+	int newHeight = baseHeight / scaleFactor;
+
+	SDL_SetWindowSize(window, newWidth, newHeight);
+
+	width = newWidth;
+	height = newHeight;
 
 	SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 	SDL_ShowWindow(window);
