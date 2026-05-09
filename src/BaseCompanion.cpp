f7f372e129b86bc7cca6d603961f5dcd05971fb7
ErikArgemi <erik.argemi@estudiantat.upc.edu>
2026-05-09 11:24:23 +0200

    Companion files

---
+++ src/BaseCompanion.cpp
@@ -0,0 +1,119 @@
+#include "BaseCompanion.h"
+#include "Entity.h"
+#include "Engine.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Physics.h"
+#include "EntityManager.h"
+#include "Map.h"
+#include "CombatManager.h"
+
+BaseCompanion::BaseCompanion(){}
+
+void BaseCompanion::Init(EntityType type, bool active, Vector2D position, const char* texturePath, int ID)
+{
+	this->type = type;
+	this->position = position;
+	this->active = active;
+	this->HP = HP;
+	this->ID = ID;
+	this->texturePath = texturePath;
+}
+
+BaseCompanion::~BaseCompanion() {
+
+}
+
+bool BaseCompanion::Awake() {
+	return true;
+}
+
+bool BaseCompanion::Start() {
+
+	//texture
+	texture = Engine::GetInstance().textures->Load(texturePath);
+	texH = texture->h;
+	texW = texture->w;
+	//sensor
+	pbody = Engine::GetInstance().physics->CreateRectangle(position.getX() + texW / 2, position.getY() + texH / 2, texH * 1.25, texW * 1.25, bodyType::DYNAMIC);
+	pbody->ctype = ColliderType::COMPANION;
+	pbody->listener = this;
+
+	return true;
+}
+
+bool BaseCompanion::Update(float dt)
+{
+
+	Move();
+	Draw(dt);
+
+	return true;
+}
+
+void BaseCompanion::Move() {
+
+	// Move 
+}
+
+void BaseCompanion::Draw(float dt) {
+
+	anims.Update(dt);
+	const SDL_Rect& animFrame = anims.GetCurrentFrame();
+
+	// Update render position using your PhysBody helper
+	int x, y;
+	pbody->GetPosition(x, y);
+	position.setX((float)x);
+	position.setY((float)y);
+
+	// Draw pathfinding debug
+	//pathfinding->DrawPath();
+
+	//Draw the player using the texture and the current animation frame
+	Engine::GetInstance().render->DrawTexture(texture, x - texW / 2, y - texH / 2);
+}
+
+bool BaseCompanion::CleanUp()
+{
+	LOG("Cleanup enemy");
+	Engine::GetInstance().textures->UnLoad(texture);
+	Engine::GetInstance().physics->DeletePhysBody(pbody);
+	return true;
+}
+
+bool BaseCompanion::Destroy()
+{
+	LOG("Destroying Companion");
+	active = false;
+	pendingToDelete = true;
+	return true;
+}
+
+void BaseCompanion::SetPosition(Vector2D pos) {
+	pbody->SetPosition((int)(pos.getX()), (int)(pos.getY()));
+}
+
+Vector2D BaseCompanion::GetPosition() {
+	int x, y;
+	pbody->GetPosition(x, y);
+	// Adjust for center
+	return Vector2D((float)x-texW/2,(float)y-texH/2);
+}
+
+void BaseCompanion::OnCollision(PhysBody* physA, PhysBody* physB) 
+{
+
+}
+
+void BaseCompanion::OnCollisionEnd(PhysBody* physA, PhysBody* physB)
+{
+	
+}
+
+
+
