120c7448c086b31cebf19870a4703ea08e68cba8
ErikArgemi <erik.argemi@estudiantat.upc.edu>
2026-06-09 00:46:25 +0200

    estatua

---
+++ Assets/Textures/Characters/Estatua.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Estatua" tilewidth="32" tileheight="64" tilecount="12" columns="6">
+ <image source="Estatua.png" width="192" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="400"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="5" duration="150"/>
+   <frame tileid="6" duration="150"/>
+   <frame tileid="7" duration="150"/>
+   <frame tileid="8" duration="150"/>
+   <frame tileid="9" duration="150"/>
+   <frame tileid="10" duration="150"/>
+   <frame tileid="11" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
