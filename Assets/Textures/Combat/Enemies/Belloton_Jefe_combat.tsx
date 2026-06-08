08b2bcdc899a9268f73915cf01ae36441a075dc5
pauot16 <pau.gallego.guerrero@estudiantat.upc.edu>
2026-06-08 17:34:49 +0200

    all animations

---
+++ Assets/Textures/Combat/Enemies/Belloton_Jefe_combat.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Belloton_Jefe_combat" tilewidth="172" tileheight="240" tilecount="4" columns="4">
+ <image source="Belloton_Jefe.png" width="688" height="240"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Assets/Textures/Combat/Enemies/Milk_combat.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Milk" tilewidth="120" tileheight="240" tilecount="6" columns="6">
+ <image source="Milk.png" width="720" height="240"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="2" duration="250"/>
+   <frame tileid="3" duration="250"/>
+   <frame tileid="4" duration="250"/>
+   <frame tileid="5" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
